687df7ffc6de3f56424ed6a77a2c056f4dbb92de
Aramis jaime <you@example.com>
2023-03-30 21:29:00 -0300

    tipado de reducer, action products, No terminado, en proceso

--- Client/src/redux/actions/productAction.ts
+++ Client/src/redux/actions/productAction.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 import {listProducts, productByID, errorMsg, carouselPicks, productsByFilters, saveTopRatedProducts, searchObject, setTodaysDiscount} from "../reducer/productReducer";
 import { LIST_PRODUCTS, LIST_PRODUCTS_BY_FILTERS } from "../../utils/constants";
+import { Game } from '../../types';
 
 //Obtener listado de productos por filtros (actual funcion de busqueda para productos. Si no sabes como se usa, pregunta a aramis o nahuel :D)
 export const getAllProducts = () => async (dispatch:any) => {
-    let arrayProducts: object[];
     try {
-        arrayProducts = (await axios.get(LIST_PRODUCTS)).data;
+        let arrayProducts:Game[] = (await axios.get(LIST_PRODUCTS)).data;
         dispatch(listProducts(arrayProducts))
     } catch (error) {
         dispatch(errorMsg("Ocurrio un error...intentelo mas tarde"))
@@ -15,9 +15,8 @@ export const getAllProducts = () => async (dispatch:any) => {
 }
 
 export const getProductsByFilters =  (filters: {},pageNumber:number) => async (dispatch: any) => {
-    let listProducts: object[];
     try{
-        listProducts = (await axios.post(`${LIST_PRODUCTS_BY_FILTERS}?pageNumber=${pageNumber}`, filters)).data;
+        let listProducts:Game[] = (await axios.post(`${LIST_PRODUCTS_BY_FILTERS}?pageNumber=${pageNumber}`, filters)).data;
         dispatch(searchObject(filters))
         dispatch(productsByFilters(listProducts));
     }catch(error){
@@ -27,9 +26,9 @@ export const getProductsByFilters =  (filters: {},pageNumber:number) => async (d
 
 //Obtener detalle de producto
 export const getProductByID =  (id: number) => async (dispatch: any) => {
-    let product: {};
     try{
-        product = (await axios.get(LIST_PRODUCTS + id)).data;
+        let product:Game = (await axios.get(LIST_PRODUCTS + id)).data;
+        
         dispatch(productByID(product));
     }catch(error){
         dispatch(errorMsg("Ocurrio un error...intentelo mas tarde"));
@@ -38,11 +37,27 @@ export const getProductByID =  (id: number) => async (dispatch: any) => {
 
 //Obtiene el listado de los productos mas gustados 
 //Datos para el carousel y el resto de juegos top que se ve en la pagina principal.
+
 export const getTopRatedProducts =  () => async (dispatch: any) => {
     try{
-        let all: object[] = (await axios.get(LIST_PRODUCTS)).data;
-        let max: object = {};
-        let maxList: object[] = [];
+        let all:Game[] = (await axios.get(LIST_PRODUCTS)).data;
+        let maxList:Game[] = [];
+        let max:Game = {
+            background_image: "",
+            description: "",
+            genres: [],
+            id: 0,
+            images: [],
+            name: "",
+            platforms: [],
+            playtime: 0,
+            price: "",
+            rating: "",
+            released: "",
+            state: false,
+            stores: [],
+        } ;
+        
         let turns:number = 0;
         let rest:number = 5;
         while (turns < 9){
@@ -59,7 +74,6 @@ export const getTopRatedProducts =  () => async (dispatch: any) => {
                 }
             })
             maxList.push(max);
-            max = {};
             rest = 5;
             turns++;
         }
@@ -67,13 +81,12 @@ export const getTopRatedProducts =  () => async (dispatch: any) => {
         dispatch(saveTopRatedProducts(maxList.slice(3, 9)));
     }catch(error){
         dispatch(errorMsg("Ocurrio un error...intentelo mas tarde"));
-        //console.log()("Exception - listProducts: " + error);
     }
 }
 
 export const setGlobalDiscount = () => async (dispatch:any) => {
     try{
-        let dayName = (new Date).toString().slice(0,3);
+        let dayName:string = (new Date).toString().slice(0,3);
         switch (dayName) {
             case 'Mon':
                 dispatch(setTodaysDiscount({genre: 'Action', discount: 20}))
--- Client/src/redux/interfaces/productInterface.ts
+++ Client/src/redux/interfaces/productInterface.ts
@@ -1,12 +1,26 @@
 import { Game } from "../../types";
 
+interface OrderFiltersTypes {
+    alphabetic:string
+    price:string
+}
+interface FiltersOrder {
+    genres: number[]
+    platform:number[]
+    priceRange:number[]
+}
+interface SearchObjTypes {
+    name:string
+    filters: FiltersOrder
+    order: OrderFiltersTypes
+}
 export interface productReducerState{
     allProductsData: Game[]
     searchedData: Array<object>,
     details: object,
-    topProductsData: Array<object>,
-    carouselData: Array<object>,
-    searchObject: object
+    topProductsData: Game[],
+    carouselData: Game[],
+    searchObject: SearchObjTypes | {}
     searchedName: string,
     selectedFilterGenreData: Array<object>,
     selectedFilterPlatformData: Array<object>,
@@ -14,9 +28,9 @@ export interface productReducerState{
     selectedAlphabeticOrderData: string,
     selectedPriceOrderData: string,
     todaysDiscount: object,
-
     discountGloballyApplied: boolean,
     adminDiscount: boolean,
     successMsg: string,
     errorMsg: string
 }
+
